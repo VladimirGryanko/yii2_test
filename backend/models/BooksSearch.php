582502ae07203b139e2cb8164a13ec7e2917dea2
Vladimir Gryanko <gryanko20012001@mail.ru>
2021-01-21 19:16:41 +0300

    CRUD операции для сущностей

---
+++ backend/models/BooksSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Books;
+
+/**
+ * BooksSearch represents the model behind the search form of `common\models\Books`.
+ */
+class BooksSearch extends Books
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'id_authors'], 'integer'],
+            [['title', 'genre'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Books::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'id_authors' => $this->id_authors,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'genre', $this->genre]);
+
+        return $dataProvider;
+    }
+}
